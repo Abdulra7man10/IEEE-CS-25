5b8d6bb221e452f0ce36a5820735c1eb26e8a91b
Abdulra7man10 <abdo144418@gmail.com>
2025-03-23 23:49:53 +0200

    task13 only 2/3 of task-1

--- Frontend/JS/main.js
+++ Frontend/JS/main.js
@@ -1,22 +1,5 @@
-/*
-  String Challenge
-  All Solutions Must Be In One Chain
-  You Can Use Concatenate
-*/
+let calculator = function (num1, num2) {
+  return num1 + num2;
+};
 
-let a = "Elzero Web School";
-
-// Include This Method In Your Solution [slice, charAt]
-console.log(a.charAt(2).toUpperCase() + a.slice(3, 6)); // Zero
-
-// 8 H
-console.log("H".repeat(8)); // HHHHHHHH
-
-// Return Array
-console.log(a.slice(0,6).split("")); // ["Elzero"]
-
-// Use Only "substr" Method + Template Literals In Your Solution
-console.log(a.substr(0, 6) + ` ` + a.substr(11, 6)); // Elzero School
-
-// Solution Must Be Dynamic Because String May Changes
-console.log(a[0].toLowerCase() + a.slice(1).toUpperCase()); // eLZERO WEB SCHOOl
+console.log(calculator(10, 20));
