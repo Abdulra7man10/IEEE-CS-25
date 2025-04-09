489dfe6bece718904754724d957c7d2c1b850ea4
Abdulra7man10 <abdo144418@gmail.com>
2025-04-09 18:54:15 +0200

    task15 is finished

--- Frontend/JS/main.js
+++ Frontend/JS/main.js
@@ -1,18 +1,1 @@
 
-for (let i=1; i<=100; i++) {
-  let myMainElement = document.createElement("div");
-  let myHeader = document.createElement("h2");
-  let myParagraph = document.createElement("p");
-
-  let myHeaderText = document.createTextNode(`Product Title ${i}`);
-  let myParagraphText = document.createTextNode("Product Description");
-
-  myHeader.appendChild(myHeaderText);
-  myMainElement.appendChild(myHeader);
-
-  myParagraph.appendChild(myParagraphText);
-  myMainElement.appendChild(myParagraph);
-
-  myMainElement.className = "product";
-  document.body.appendChild(myMainElement);
-}
